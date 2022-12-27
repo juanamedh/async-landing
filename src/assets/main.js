43760aa9425ce8818a2d538bdc8dbbcd106e403c
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-12-26 18:32:51 -0800

    add API snipped

---
+++ src/assets/main.js
@@ -0,0 +1,14 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?relatedToVideoId=7ghhRHRP6t4&part=id%2Csnippet&type=video&maxResults=50';
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e4a2ebb7e9mshdff1ef157c3876dp1e1c22jsnfecd45a089a7',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+fetch('', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
