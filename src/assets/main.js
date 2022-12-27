679831d63d447d0d341e3a5354727fa37a6ffe2e
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-12-26 20:15:26 -0800

    fixed template title bug

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,11 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?relatedToVideoId=7ghhRHRP6t4&part=id%2Csnippet&type=video&maxResults=50';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCF3Ez6QwZwwr_E7RZGJMW0A&part=snippet%2Cid&order=date&maxResults=9';
 
 const content = null || document.getElementById('content');
 
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'e4a2ebb7e9mshdff1ef157c3876dp1e1c22jsnfecd45a089a7',
+		'X-RapidAPI-Key': '70a1f3dec9mshd06c0b1cf9ef258p1c506djsnd2588384a27b',
 		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
 	}
 };
@@ -21,18 +21,18 @@ async function fetchData(urlAPI) {
 		const videos = await fetchData(API);
 		let view = `
     ${videos.items.map(video => `
-      <div class="group relative">
-        <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-        </div>
-        <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-gray-700">
-            <span aria-hidden="true" class="absolute inset-0"><span>
-            ${video.snippet.title}
-          </h3>
-        </div>
-      </div>
-    `).slice(0,4).join('')}
+    <div class="group relative">
+      <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+      <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+    </div>
+    <div class="mt-4 flex justify-between">
+      <h3 class="text-sm text-gray-700">
+        <span aria-hidden="true" class="absolute inset-0"></span>
+        ${video.snippet.title}
+      </h3>
+    </div>
+  </div>
+    `).slice(0, 4).join('')}
 		`;
     content.innerHTML = view;
 	}catch (error) {
