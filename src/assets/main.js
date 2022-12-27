aa7d670ea5596c080ba754bc5b3afb86e6b28a39
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-12-26 19:02:42 -0800

    set parameter into template

--- src/assets/main.js
+++ src/assets/main.js
@@ -18,17 +18,20 @@ async function fetchData=(urlAPI) {
 	try{
 		const videos = await fetchData(API);
 		let view = `
-			<div class="group relative">
+    ${videos.items.map(video => `
+      <div class="group relative">
         <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="" alt="" class="w-full">
+          <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
           <h3 class="text-sm text-gray-700">
             <span aria-hidden="true" class="absolute inset-0"><span>
-            title
+            ${video.snippet.title}
           </h3>
         </div>
       </div>
+    `).slice(0,4).join('')}
+
 		`;
 	}catch {
 
