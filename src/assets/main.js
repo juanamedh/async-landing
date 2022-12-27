363f30bc861817481dbb50e81919611559407e0e
Juan Sanchez <58664932+juanamedh@users.noreply.github.com>
2022-12-26 18:53:45 -0800

    add html template

--- src/assets/main.js
+++ src/assets/main.js
@@ -17,7 +17,19 @@ async function fetchData=(urlAPI) {
 (async () => {
 	try{
 		const videos = await fetchData(API);
-		
+		let view = `
+			<div class="group relative">
+        <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="" alt="" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"><span>
+            title
+          </h3>
+        </div>
+      </div>
+		`;
 	}catch {
 
 	}
